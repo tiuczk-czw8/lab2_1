b1f35147bda2ea25ac58601bafab854458ceff66
Marcin Kotulski <233000@edu.p.lodz.pl>
2019-11-14 09:49:21 +0100

    gitignore upadte for IDE

---
+++ src/test/java/BinarySearchTests.java
@@ -0,0 +1,12 @@
+import junit.framework.Assert;
+import edu.iis.mto.bsearch.BinarySearch;
+import edu.iis.mto.bsearch.SearchResult;
+public class Test {
+
+    @org.junit.Test
+    public void seqOneElement(){
+
+        SearchResult searchResult= new SearchResult();
+        searchResult.
+    }
+}
