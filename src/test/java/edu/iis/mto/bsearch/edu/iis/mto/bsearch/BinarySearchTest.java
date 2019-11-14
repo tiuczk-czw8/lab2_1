9e68a2f4c46361857802fa9cca0905ec217648c0
Justyna <jkloc96@gmail.com>
2019-11-14 11:20:12 +0100

    testy_v1 - added new test cases

--- src/test/java/edu/iis/mto/bsearch/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -39,8 +39,37 @@ public class BinarySearchTest {
 	public void searchElementNotInSequenceLengthGreaterThan1() {
 		int key = 10;
 		int[] seq = {21,3,100};
+		
 		SearchResult result = BinarySearch.search(key, seq);
+		
 		assertThat(result.isFound(), Matchers.equalTo(false));
 	}
+	
+	@Test 
+	public void searchElementIsFirstInSequenceLengthGreaterThan1() {
+		int key = 4;
+		int[] seq = new int[]{key,5,21,100};
+		int index = 0; 
+		
+		SearchResult result = BinarySearch.search(key, seq);
+		
+		Assert.assertTrue(result.isFound());
+		Assert.assertEquals(index,result.getPosition());
+		Assert.assertEquals(key, seq[index]);
+	}
+	
+	@Test
+	public void searchElementIsLastInSequenceLengthGreaterThan1() {
+		int key = 102;
+		int[ seq = new int[] {3,21,100,key};
+		int index = 3;
+		
+		SearchResult result = BinarySearch.search(key, seq);
+		
+		Assert.assertTrue(result.isFound());
+		Assert.assertEquals(index,result.getPosition());
+		Assert.assertEquals(key, seq[index]);
+	}
+
 	
 }
