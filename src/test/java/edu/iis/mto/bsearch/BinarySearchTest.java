03448c9238fa66fa887df7f9c8fc0fd049e9d194
Mike <michaj96@gmail.com>
2019-11-14 10:42:34 +0100

    testy_v1 searching of the first element

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -23,4 +23,11 @@ public class BinarySearchTest {
         assertThat(searchResult.isFound(), Matchers.equalTo(false));
     }
 
+    @Test
+    public void searchFirstElementFound() {
+        int key = 10;
+        int[] seq = {10, 11, 12};
+        SearchResult searchResult = BinarySearch.search(key, seq);
+        assertThat(searchResult.getPosition(), Matchers.equalTo(1));
+    }
 }
