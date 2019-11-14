497bc0bfaeb9ade6eec424b724e0a45cacb5accc
Justyna <jkloc96@gmail.com>
2019-11-14 11:04:38 +0100

    testy_v1 - added new test cases

--- src/test/java/edu/iis/mto/bsearch/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -3,6 +3,7 @@ package edu.iis.mto.bsearch.edu.iis.mto.bsearch;
 import static org.junit.Assert.*;
 
 import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Test;
 
 import edu.iis.mto.bsearch.BinarySearch;
@@ -11,11 +12,35 @@ import edu.iis.mto.bsearch.SearchResult;
 public class BinarySearchTest {
 
 	@Test
-	public void searchElementnotfound() {
+	public void searchElementInSequenceLength1() {
+		int key = 2;
+		int[] seq = new int[] {key};
+		int index = 0;
+		
+		SearchResult result = BinarySearch.search(key, seq);
+		
+		Assert.assertTrue(result.isFound());
+		Assert.assertEquals(index, result.getPosition());
+		Assert.assertEquals(key,seq[index]);		
+	}
+	
+	@Test 
+	public void SearchElementNotInSequenceLength1() {
+		int key = 3;
+		int[] seq = new int[] {3};
+		
+		SearchResult result = BinarySearch.search(key, seq);
+		
+		Assert.assertFalse(result.isFound());
+		Assert.assertEquals(-1,result.getPosition());
+	}
+	
+	@Test
+	public void searchElementNotInSequenceLengthGreaterThan1() {
 		int key = 10;
 		int[] seq = {21,3,100};
-		SearchResult SearchResult = BinarySearch.search(key, seq);
-		assertThat(SearchResult.isFound(), Matchers.equalTo(false));
+		SearchResult result = BinarySearch.search(key, seq);
+		assertThat(result.isFound(), Matchers.equalTo(false));
 	}
-
+	
 }
