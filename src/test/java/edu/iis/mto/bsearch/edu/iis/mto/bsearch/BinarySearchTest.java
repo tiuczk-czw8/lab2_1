9b3fcad88f58a592c6f19848f15445f919d1324a
Justyna <jkloc96@gmail.com>
2019-11-14 09:54:08 +0100

    testy_v1 - beginning

---
+++ src/test/java/edu/iis/mto/bsearch/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -0,0 +1,14 @@
+package edu.iis.mto.bsearch.edu.iis.mto.bsearch;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BinarySearchTest {
+
+	@Test
+	public void BinarySearchTest() {
+		fail("Not yet implemented");
+	}
+
+}
