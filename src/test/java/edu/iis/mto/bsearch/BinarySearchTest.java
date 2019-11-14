91aa2bd011ee3b01e76f1db26583882ee925f72e
202156 <202156@CARBON.CTI.P.LODZ.PL>
2019-11-14 09:15:24 +0100

    Second-Test-Passed-For-Single-Element

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -1,15 +1,23 @@
 package edu.iis.mto.bsearch;
 
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class BinarySearchTest {
 
     @Test
-    public void search() {
+    public void searchSingleElementTrue() {
         int key = 25;
         int[] seq = {25};
         SearchResult searchResult = BinarySearch.search(key, seq);
         assertTrue(searchResult.isFound());
+    }
+
+    @Test
+    public void searchSingleElementFalse() {
+        int key = 25;
+        int[] seq = {24};
+        SearchResult searchResult = BinarySearch.search(key, seq);
+        assertFalse(searchResult.isFound());
     }
 }
