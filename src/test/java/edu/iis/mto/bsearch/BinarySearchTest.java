adb7a0cbcb564768cb10279aecb5347de9682b46
Mike <michaj96@gmail.com>
2019-11-14 10:47:45 +0100

    testy_v1 searching of the middle element

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -38,4 +38,12 @@ public class BinarySearchTest {
         SearchResult searchResult = BinarySearch.search(key, seq);
         assertThat(searchResult.getPosition(), Matchers.equalTo(3));
     }
+
+    @Test
+    public void searchMiddleElementFound() {
+        int key = 8;
+        int[] seq = {9, 8, 10};
+        SearchResult searchResult = BinarySearch.search(key, seq);
+        assertThat(searchResult.getPosition(), Matchers.equalTo(2));
+    }
 }
