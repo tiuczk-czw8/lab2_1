8713c96bb159dc8a155a11595bfaf2320dcac25d
Strzepek.Aleksander <233010@edu.p.lodz.pl>
2019-11-14 09:45:49 +0100

    testy_v1 assumed test cases implemented

---
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -0,0 +1,24 @@
+package edu.iis.mto.bsearch;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class BinarySearchTest {
+
+    @Test
+    public void assumedTestCases() {
+        testBinarySearch(1, new int[] {1}, true, 0);
+        testBinarySearch(1, new int[] {2}, false, -1);
+        testBinarySearch(1, new int[] {1, 2, 3, 4}, true, 0);
+        testBinarySearch(4, new int[] {1, 2, 3, 4}, true, 3);
+        testBinarySearch(3, new int[] {1, 2, 3, 4, 5}, true, 2);
+        testBinarySearch(6, new int[] {1, 2, 3, 4, 5}, false, -1);
+    }
+
+    public void testBinarySearch(int key, int[] seq, boolean isFound, int position) {
+        assertEquals(BinarySearch.search(key, seq).isFound(), isFound);
+        assertEquals(BinarySearch.search(key, seq).getPosition(), position);
+    }
+
+}
