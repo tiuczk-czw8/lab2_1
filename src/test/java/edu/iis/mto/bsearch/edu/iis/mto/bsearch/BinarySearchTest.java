c01d0cc06e50f2fa17bac0f6eac97fcbeb965808
Justyna <jkloc96@gmail.com>
2019-11-14 10:41:11 +0100

    testy_v1 - not found

--- src/test/java/edu/iis/mto/bsearch/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -2,13 +2,20 @@ package edu.iis.mto.bsearch.edu.iis.mto.bsearch;
 
 import static org.junit.Assert.*;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
+
+import edu.iis.mto.bsearch.BinarySearch;
+import edu.iis.mto.bsearch.SearchResult;
 
 public class BinarySearchTest {
 
 	@Test
-	public void BinarySearchTest() {
-		fail("Not yet implemented");
+	public void searchElementnotfound() {
+		int key = 10;
+		int[] seq = {21,3,100};
+		SearchResult SearchResult = BinarySearch.search(key, seq);
+		assertThat(SearchResult.isFound(), Matchers.equalTo(false));
 	}
 
 }
