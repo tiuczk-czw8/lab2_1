4b57e4781d4ffd65b65325b8b44b0c5d3a510c73
202156 <202156@CARBON.CTI.P.LODZ.PL>
2019-11-14 09:47:23 +0100

    Fourth-Test-Passed-For-Single-Element

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -28,4 +28,12 @@ public class BinarySearchTest {
         SearchResult searchResult = BinarySearch.search(key, seq);
         assertEquals(searchResult.getPosition(), 1);
     }
+
+    @Test
+    public void searchLastElement() {
+        int key = 27;
+        int[] seq = {24, 25, 27};
+        SearchResult searchResult = BinarySearch.search(key, seq);
+        assertEquals(searchResult.getPosition(), 3);
+    }
 }
