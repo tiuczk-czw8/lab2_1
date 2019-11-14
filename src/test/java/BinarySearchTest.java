db9e2fe37eef75c72490c6fbf8dbd9982cf65ef8
Domastos <42493112+Domastos@users.noreply.github.com>
2019-11-14 09:55:35 +0100

    testy_v1 BinarySearchTest created

---
+++ src/test/java/BinarySearchTest.java
@@ -0,0 +1,19 @@
+import edu.iis.mto.bsearch.BinarySearch;
+import edu.iis.mto.bsearch.SearchResult;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BinarySearchTest {
+    @Test
+    public void shouldElementBeInSequenceLength1() {
+        int key = 2;
+        int[] seq = new int[]{key};
+        int idx = 0;
+
+        SearchResult result = BinarySearch.search(key, seq);
+
+        Assert.assertTrue(result.isFound());
+        Assert.assertEquals(idx, result.getPosition());
+        Assert.assertEquals(key, seq[idx]);
+    }
+}
