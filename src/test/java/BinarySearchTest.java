7fe7ab7e063d7f4c6e782fdfdd637498c1222396
Paweł Siciński <233008@edu.p.lodz.pl>
2019-11-17 20:40:34 +0100

    testy_v2: Testing preconditions, altering binarySearch() due to lack of exception thrown.

--- src/test/java/BinarySearchTest.java
+++ src/test/java/BinarySearchTest.java
@@ -1,4 +1,5 @@
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.hasItemInArray;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
@@ -9,6 +10,7 @@ import static org.junit.Assert.assertThat;
 import edu.iis.mto.bsearch.BinarySearch;
 import edu.iis.mto.bsearch.SearchResult;
 import java.util.Arrays;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
 
@@ -36,6 +38,44 @@ public class BinarySearchTest {
         assertThat(getBoxedArray(seq), not(hasItemInArray(key)));
     }
 
+    private static void testPreconditions(@NotNull int[] seq) {
+        assertThat(seq.length, greaterThan(0));
+        assertThat(hasConsecutiveValues(seq), is(true));
+        assertThat(hasDistinctValues(seq), is(true));
+    }
+
+    @Contract(pure = true)
+    private static boolean hasConsecutiveValues(@NotNull int[] seq) {
+        if (seq.length == 1) {
+            return true;
+        }
+
+        int previousValue = seq[0];
+
+        for (int i = 1; i < seq.length; i++) {
+            if (seq[i] <= previousValue) {
+                return false;
+            }
+
+            previousValue = seq[i];
+        }
+
+        return true;
+    }
+
+    @Contract(pure = true)
+    private static boolean hasDistinctValues(@NotNull int[] seq) {
+        for (int i = 0; i < seq.length; i++) {
+            for (int j = i + 1; j < seq.length - 1; j++) {
+                if (seq[i] == seq[j]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
     @Test
     public void elementShouldBeInSequenceLength1() {
         int key = 2;
@@ -44,6 +84,7 @@ public class BinarySearchTest {
 
         SearchResult result = BinarySearch.search(key, seq);
 
+        testPreconditions(seq);
         testResultForTrueAssumptions(result, seq, key, idx);
     }
 
@@ -54,6 +95,7 @@ public class BinarySearchTest {
 
         SearchResult result = BinarySearch.search(key, seq);
 
+        testPreconditions(seq);
         testResultForFalseAssumptions(result, seq, key);
     }
 
@@ -65,6 +107,7 @@ public class BinarySearchTest {
 
         SearchResult result = BinarySearch.search(key, seq);
 
+        testPreconditions(seq);
         testResultForTrueAssumptions(result, seq, key, idx);
     }
 
@@ -76,6 +119,7 @@ public class BinarySearchTest {
 
         SearchResult result = BinarySearch.search(key, seq);
 
+        testPreconditions(seq);
         testResultForTrueAssumptions(result, seq, key, idx);
     }
 
@@ -87,6 +131,7 @@ public class BinarySearchTest {
 
         SearchResult result = BinarySearch.search(key, seq);
 
+        testPreconditions(seq);
         testResultForTrueAssumptions(result, seq, key, idx);
     }
 
@@ -97,6 +142,14 @@ public class BinarySearchTest {
 
         SearchResult result = BinarySearch.search(key, seq);
 
+        testPreconditions(seq);
         testResultForFalseAssumptions(result, seq, key);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionForSequenceLengthZero() {
+        int[] seq = new int[] {};
+
+        BinarySearch.search(0, seq);
     }
 }
