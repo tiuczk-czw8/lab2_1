1d2674c18e8d239a043c718010ee2e29db399f50
yevheniinokhrin <233315@edu.p.lodz.pl>
2019-11-20 18:58:29 +0100

    testy_v1 Tests are separated to several methods

--- src/test/java/TestSeachResult.java
+++ src/test/java/TestSeachResult.java
@@ -4,27 +4,31 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TestSeachResult {
-@Test
-public  void testSearch()
-{
-
+    @Test
+    public  void testOneElSearch()
+    {
     testBoth(15,1,true,new int[]{15});
     testBoth(2,-1,false,new int[]{15});
-    testBoth(12,5,true, new int[]{0,2,5,7,12});
-    testBoth(4,1,true, new int[]{4,8,11,12});
-    testBoth(11,3,true, new int[]{4,8,11,12});
-    testBoth(6,-1,false, new int[]{4,8,11,12});
-    
-}
+
+    }
+    @Test
+    public  void testSeveralElSearch()
+    {
+        testBoth(12,5,true, new int[]{0,2,5,7,12});
+        testBoth(4,1,true, new int[]{4,8,11,12});
+        testBoth(11,3,true, new int[]{4,8,11,12});
+        testBoth(6,-1,false, new int[]{4,8,11,12});
+    }
+
 
 public SearchResult result(int key, int[] arr)
 {
     return  BinarySearch.search(key, arr);
 }
 
-public void testBoth(int el, int possition, boolean isFound, int[] arr)
+public void testBoth(int el, int position, boolean isFound, int[] arr)
 {
     Assert.assertEquals(isFound, result(el, arr).isFound());
-    Assert.assertEquals(possition, result(el, arr).getPosition());
+    Assert.assertEquals(position, result(el, arr).getPosition());
 }
 }
