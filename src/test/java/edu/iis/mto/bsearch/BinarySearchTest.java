d0a78ebda4057fded4163d94fc1e494876212355
Krystian Bąk <krystianb.bk@gmail.com>
2019-11-20 14:59:14 +0100

    testy_v2 dodany test gdy sprawdzana jest pusta tablica, dodana tez refaktoryzacja kodu

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -1,59 +1,75 @@
 package edu.iis.mto.bsearch;
 
-import org.graalvm.compiler.graph.spi.Canonicalizable;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BinarySearchTest {
+	private int key;
+	private int[] set;
 
     @Test
-    public void searchInSeqTrue() {
-        int key = 25;
-        int[] seq={25};
-        assertThat(BinarySearch.search(key,seq).isFound(), Matchers.equalTo(true));
+    public void searchInSingleSeqTrue() {
+        key = 25;
+        set=new int[]{25};
+        isInSequence(key,set,true);
     }
 
     @Test
-    public void searchInSeqFalse() {
-        int key = 24;
-        int[] seq = {25};
-        assertThat(BinarySearch.search(key,seq).isFound(), Matchers.equalTo(false));
+    public void searchInSingleSeqFalse() {
+        key = 24;
+        set = new int[]{25};
+        isInSequence(key,set,false);
     }
 
     @Test
     public void searchInMultiSeqFirst()
     {
-        int key = 25;
-        int[] set = {25,26,27};
+    	key = 25;
+        set = new int[]{25,26,27};
         SearchResult result = BinarySearch.search(key,set);
-        assertThat(result.getPosition(),Matchers.equalTo(1));
+        isInExpectedPosition(key,set,1);
     }
 
     @Test
     public void searchInMultiSeqLast()
     {
-        int key = 27;
-        int[] set = {25,26,27};
+        key = 27;
+        set = new int[]{25,26,27};
         SearchResult result = BinarySearch.search(key,set);
-        assertThat(result.getPosition(),Matchers.equalTo(3));
+        isInExpectedPosition(key,set,3);
     }
 
     @Test
     public void searchInMultiSeqMiddle() {
-        int key = 26;
-        int[] set = {25,26,27};
+        key = 26;
+        set = new int[]{25,26,27};
         SearchResult result = BinarySearch.search(key,set);
-        assertThat(result.getPosition(),Matchers.equalTo(2));
+        isInExpectedPosition(key,set,2);
     }
 
     @Test
     public void searchInMultiSeqNotFound() {
-        int key = 28;
-        int[] set = {25,26,27};
-        assertThat(BinarySearch.search(key,set).isFound(), Matchers.equalTo(false));
+        key = 28;
+        set = new int[]{25,26,27};
+        isInSequence(key,set,false);
     }
+    
+    @Test (expected = IllegalArgumentException.class)
+    public void searchInEmptySeq() {
+    	key = 25;
+    	set = new int[]{};
+    	BinarySearch.search(key, set);
 
+    }
+    
+    private void isInExpectedPosition(int key, int[] seq, int position) {
+    	assertThat(BinarySearch.search(key,seq).getPosition(),Matchers.equalTo(position));
+    }
+    
+    private void isInSequence(int key, int[] seq, boolean isIt) {
+    	assertThat(BinarySearch.search(key,seq).isFound(),Matchers.equalTo(isIt));
+    }
 
 }
