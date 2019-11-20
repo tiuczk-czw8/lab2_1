0e20a5bfa34066ac5084d42a12fcd901f3c01840
Domastos <isavchenko18@gmail.com>
2019-11-21 00:22:52 +0100

    testy_v1 First and Last elements test
    Created FirstElementInSeq test and passed
    Created LastElementInSeq test and passed

--- src/test/java/BinarySearchTest.java
+++ src/test/java/BinarySearchTest.java
@@ -40,4 +40,30 @@ public class BinarySearchTest {
         Assert.assertEquals(idx, result.getPosition());
         Assert.assertEquals(key, seq[idx]);
     }
+
+    @Test
+    public void FirstElementInSeq() {
+        int key = 2;
+        int[] seq = new int[]{key, 3, 4};
+        int idx = 0;
+
+        SearchResult result = BinarySearch.search(key, seq);
+
+        Assert.assertTrue(result.isFound());
+        Assert.assertEquals(idx, result.getPosition());
+        Assert.assertEquals(key, seq[idx]);
+    }
+
+    @Test
+    public void  LastElementInSeq() {
+        int key = 4;
+        int[] seq = new int[]{1, 2, 3, 4};
+        int idx = 3;
+
+        SearchResult result = BinarySearch.search(key, seq);
+
+        Assert.assertTrue(result.isFound());
+        Assert.assertEquals(idx, result.getPosition());
+        Assert.assertEquals(key, seq[idx]);
+    }
 }
