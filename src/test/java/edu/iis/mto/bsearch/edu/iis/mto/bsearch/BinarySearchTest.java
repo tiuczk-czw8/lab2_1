22de79147fcf2b716e2f632924e3c20d241e4ca1
Justyna <jkloc96@gmail.com>
2019-11-20 21:25:15 +0100

    testy_v3

--- src/test/java/edu/iis/mto/bsearch/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -1,6 +1,7 @@
 package edu.iis.mto.bsearch.edu.iis.mto.bsearch;
 
 import static org.junit.Assert.*;
+
 import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.Test;
@@ -11,11 +12,58 @@ import edu.iis.mto.bsearch.SearchResult;
 public class BinarySearchTest {
 
 	@Test(expected = IllegalArgumentException.class)
-	public void sequenceisEmpty() {
+	public void sequenceIsEmpty() {
 		int key = 2;
 		int[] seq = new int[] {};
 		
 		BinarySearch.search(key, seq);	
+	}
+	
+	@Test
+	public void sequenceIsNotSorted() {
+		int key = 3;
+		int[] seq = new int[] {32,4,3,87};
+		
+		SearchResult result = BinarySearch.search(key, seq);	
+		
+		assertThat(seq.length, is(not(1)));
+		assertThat(result.getPosition(), is(-1));
+	}
+	
+	@Test
+	public void searchElementIsInMiddleWhensequenceIsNotEven() {
+		int key = 5;
+		int[] seq = new int[] {1,2,3,4,5,6,7,8,9,10};
+		
+		SearchResult result = BinarySearch.search(key, seq);	
+		
+		assertThat(result.getPosition(), equalTo((int) Math.ceil(seq.length / 2.0)));
+	}
+	
+	@Test
+	public void isTwoSequencesTrue() {
+		int key = 3;
+		int[] seq = new int[] {3,4,5,87};
+		int key1 = 3;
+		int[] seq1 = new int[] {3,4,5,87};
+		
+		SearchResult result = BinarySearch.search(key, seq);
+		SearchResult result1 = BinarySearch.search(key1, seq1);
+		
+		assertThat(result1.equals(result), is(true));	
+	}
+	
+	@Test
+	public void isTwoSequencesFalse() {
+		int key = 3;
+		int[] seq = new int[] {3,4,5,87};
+		int key1 = 4;
+		int[] seq1 = new int[] {3,4,5,87};
+		
+		SearchResult result = BinarySearch.search(key, seq);
+		SearchResult result1 = BinarySearch.search(key1, seq1);
+		
+		assertThat(result1.equals(result), is(false));	
 	}
 	
 	@Test
