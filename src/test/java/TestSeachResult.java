59ef1cf15c439e0a3549c44ef7b25ec7974e7fb8
yevheniinokhrin <233315@edu.p.lodz.pl>
2019-11-20 19:20:13 +0100

    testy_v1 Refactored BinarySearch code

--- src/test/java/TestSeachResult.java
+++ src/test/java/TestSeachResult.java
@@ -7,28 +7,28 @@ public class TestSeachResult {
     @Test
     public  void testOneElSearch()
     {
-    testBoth(15,1,true,new int[]{15});
-    testBoth(2,-1,false,new int[]{15});
+        testBoth(15,0,true,new int[]{15});
+        testBoth(2,-1,false,new int[]{15});
 
     }
     @Test
     public  void testSeveralElSearch()
     {
-        testBoth(12,5,true, new int[]{0,2,5,7,12});
-        testBoth(4,1,true, new int[]{4,8,11,12});
-        testBoth(11,3,true, new int[]{4,8,11,12});
+        testBoth(12,4,true, new int[]{0,2,5,7,12});
+        testBoth(4,0,true, new int[]{4,8,11,12});
+        testBoth(11,2,true, new int[]{4,8,11,12});
         testBoth(6,-1,false, new int[]{4,8,11,12});
     }
 
 
-public SearchResult result(int key, int[] arr)
-{
-    return  BinarySearch.search(key, arr);
-}
+    public SearchResult result(int key, int[] arr)
+    {
+        return  BinarySearch.search(key, arr);
+    }
 
-public void testBoth(int el, int position, boolean isFound, int[] arr)
-{
-    Assert.assertEquals(isFound, result(el, arr).isFound());
-    Assert.assertEquals(position, result(el, arr).getPosition());
-}
+    public void testBoth(int el, int position, boolean isFound, int[] arr)
+    {
+        Assert.assertEquals(isFound, result(el, arr).isFound());
+        Assert.assertEquals(position, result(el, arr).getPosition());
+    }
 }
