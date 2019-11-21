5a9be4b1fe6b59b4c13d6b9d85eba6a780d2904d
xyz <xyz@example.com>
2019-11-21 02:08:26 +0100

    testy_v2 New tests added

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -6,15 +6,38 @@ import org.hamcrest.Matchers;
 import org.junit.Test;
 
 public class BinarySearchTest {
+    
+    @Test
+    public void elementIn1ElementSequence() {
+        testBinarySearch(1, new int[] {1}, true, 0);
+    }
 
     @Test
-    public void assumedTestCases() {
-        testBinarySearch(1, new int[] {1}, true, 0);
+    public void elementNotIn1ElementSequence() {
         testBinarySearch(1, new int[] {2}, false, -1);
-        testBinarySearch(1, new int[] {1, 2, 3, 4}, true, 0);
-        testBinarySearch(4, new int[] {1, 2, 3, 4}, true, 3);
-        testBinarySearch(3, new int[] {1, 2, 3, 4, 5}, true, 2);
+    }
+
+    @Test
+    public void elementNotInSequence() {
         testBinarySearch(6, new int[] {1, 2, 3, 4, 5}, false, -1);
+        testBinarySearch(9, new int[] {1, 2, 3, 4, 5, 6}, false, -1);
+        testBinarySearch(3, new int[] {1, 2, 4, 4, 5}, false, -1);
+    }
+
+    @Test
+    public void firstElementInSequence() {
+        testBinarySearch(1, new int[] {1, 2, 3, 4}, true, 0);
+    }
+
+    @Test
+    public void lastElementInSequence() {
+        testBinarySearch(4, new int[] {1, 2, 3, 3, 3, 3, 4}, true, 6);
+    }
+
+    @Test
+    public void middleElementInSequence() {
+        testBinarySearch(2, new int[] {1, 2, 3}, true, 1);
+        testBinarySearch(3, new int[] {1, 2, 3, 4, 5}, true, 2);
     }
 
     public void testBinarySearch(int key, int[] seq, boolean isFound, int position) {
