6b71479cf27e1fbb0b4ad5e8ead22c06ca02abfb
KonradKaniecki <konrad.kaniecki96@gmail.com>
2019-11-24 19:55:06 +0100

    Test v1 Czy element jest w sekwencji

---
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -0,0 +1,16 @@
+package edu.iis.mto.bsearch;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+public class BinarySearchTest {
+    @Test
+    public void searchSingleElementTrue() {
+        int key = 50;
+        int[] seq = {50};
+        SearchResult searchResult = BinarySearch.search(key, seq);
+        assertThat(searchResult.isFound(), Matchers.equalTo(true));
+    }
+
+
+}
