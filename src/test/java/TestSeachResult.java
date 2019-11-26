6675b30012d9f15905c2aeafb1952f1f214e3ee8
yevheniinokhrin <233315@edu.p.lodz.pl>
2019-11-26 01:01:34 +0100

    Added more test methods

--- src/test/java/TestSeachResult.java
+++ src/test/java/TestSeachResult.java
@@ -7,27 +7,48 @@ import org.junit.Test;
 
 public class TestSeachResult {
     @Test
-    public  void testOneElSearch()
+    public  void testOneElInSeqSearch()
     {
         testBoth(15,0,true,new int[]{15});
-        testBoth(2,-1,false,new int[]{15});
-
     }
     @Test
-    public  void testSeveralElSearch()
+    public  void testOneElNotInSeqSearch()
+    {
+        testBoth(2,-1,false,new int[]{15});
+    }
+
+    @Test
+    public  void testSeveralFirstElSearch()
+    {
+        testBoth(4,0,true, new int[]{4,8,11,12});
+    }
+    @Test
+    public  void testSeveralLastElSearch()
     {
         testBoth(12,4,true, new int[]{0,2,5,7,12});
-        testBoth(5,2,true, new int[]{0,2,5,7,12});
-        testBoth(4,0,true, new int[]{4,8,11,12});
+    }
+    @Test
+    public  void testSeveralMidElEvenSeqSearch()
+    {
         testBoth(11,2,true, new int[]{4,8,11,12});
+    }
+    @Test
+    public  void testSeveralMidElNotEvenSeqSearch()
+    {
+        testBoth(5,2,true, new int[]{0,2,5,7,12});
+    }
+    @Test
+    public  void testSeveralElNotInSeqSearch()
+    {
         testBoth(6,-1,false, new int[]{4,8,11,12});
     }
 
-
     @Test (expected = IllegalArgumentException.class)
-    public void testEmptySeq() {
+    public void testEmptySeq()
+    {
         result(1,new int[]{});
     }
+
     public SearchResult result(int key, int[] arr)
     {
         return  BinarySearch.search(key, arr);
