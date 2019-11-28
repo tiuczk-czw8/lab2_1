2398ae2869c811a5ff2e6d57ff7c49a30b14b645
sticz1234 <sticz1234@gmail.com>
2019-11-28 14:23:46 +0100

    Test V1 Sprawdzenie, czy element znajduje się w sekwencji

---
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -0,0 +1,24 @@
+package edu.iis.mto.bsearch;
+import static org.junit.Assert.*;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BinarySearchTest {
+
+    @Test
+    public void search() {
+        int key = 60;
+        int[] seq = {60};
+        SearchResult searchResult = BinarySearch.search(key, seq);
+        assertThat(searchResult.isFound(), Matchers.equalTo(true));
+    }
+
+
+
+
+
+}
